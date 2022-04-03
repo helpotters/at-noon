f6fc26dee61c6d35aac69b2efd1fe724ab74435b
Paul Lemus <paullemus@protonmail.com>
2022-04-02 17:56:06 -0700

    spec: added Event model presence validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ApplicationRecord
+  validates_presence_of :title, :description, :start_date, :end_date
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(title: "Dungeons and Dragons Session",
+                        description: "Gary's campaign",
+                        start_date: Date.today,
+                        end_date: Date.today + 1.days)
+  }
+
+  it { is_expected.to be_valid }
+  it "is not valid without title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without start_date" do
+    subject.start_date = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without end_date" do
+    subject.end_date = nil
+    expect(subject).to_not be_valid
+  end
 end

5c910461c57f41cc18b2063af4c773ed49991502
Paul Lemus <paullemus@protonmail.com>
2022-04-05 17:45:15 -0700

    styling: changed double quotes to single

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,27 +1,27 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  subject {
-    described_class.new(title: "Dungeons and Dragons Session",
+  subject do
+    described_class.new(title: 'Dungeons and Dragons Session',
                         description: "Gary's campaign",
                         start_date: Date.today,
                         end_date: Date.today + 1.days)
-  }
+  end
 
   it { is_expected.to be_valid }
-  it "is not valid without title" do
+  it 'is not valid without title' do
     subject.title = nil
     expect(subject).to_not be_valid
   end
-  it "is not valid without description" do
+  it 'is not valid without description' do
     subject.description = nil
     expect(subject).to_not be_valid
   end
-  it "is not valid without start_date" do
+  it 'is not valid without start_date' do
     subject.start_date = nil
     expect(subject).to_not be_valid
   end
-  it "is not valid without end_date" do
+  it 'is not valid without end_date' do
     subject.end_date = nil
     expect(subject).to_not be_valid
   end

e58ebb1eaee26dc3a6344ca0ed454aa48bf4b148
Paul Lemus <paullemus@protonmail.com>
2022-04-09 19:14:32 -0700

    fix: added destroy dependency to voter model

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,5 +1,5 @@
 class Voter < ApplicationRecord
-  belongs_to :event, optional: true
+  belongs_to :event, optional: true, dependent: :destroy
   has_many :votes
 
   validates_presence_of :name, :event

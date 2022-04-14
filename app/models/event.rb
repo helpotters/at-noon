42dbbc691b6722e680cda375e110c5316b14857b
Paul Lemus <paullemus@protonmail.com>
2022-04-14 01:49:35 -0700

    fix: added destroy dependencies

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
-  has_many :voters
+  has_many :voters, dependent: :destroy
   validates_presence_of :title, :description, :start_date, :end_date
 end
--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,6 +1,6 @@
 class Voter < ApplicationRecord
-  belongs_to :event, optional: true, dependent: :destroy
-  has_many :votes
+  belongs_to :event, optional: true
+  has_many :votes, dependent: :destroy
 
   validates_presence_of :name, :event
 end

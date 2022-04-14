226509cb88999368f6f95afddd132d15d7345e8d
Paul Lemus <paullemus@protonmail.com>
2022-04-14 01:30:05 -0700

    feat: added voter name submission partial to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,23 @@
 class EventsController < ApplicationController
+  before_action :show_event, only: %i[show]
   def new
     @event = Event.new
   end
 
   def create
     @event = Event.create(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to event_url(@event), notice: 'Event created!' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @voter = Voter.new
   end
 
   def destroy
@@ -15,5 +28,9 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:title, :description, :start_date, :end_date)
+  end
+
+  def show_event
+    @event = Event.find(params[:id])
   end
 end
--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -1,21 +1,31 @@
 class VotersController < ApplicationController
-  before_action :load_event
+  # before_action :load_event
 
   def new
-    @voter = Voter.build
+    @event = Event.find(params[:event_id])
+    @voter = @event.voters.build
   end
 
   def create
+    @event = Event.find(voter_params[:event_id])
     @voter = @event.voters.build(voter_params)
+
+    respond_to do |format|
+      if @voter.save
+        format.html { redirect_to event_path(@event.id), notice: 'voter created!' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
-  def load_event
-    @event = Event.find(params[:event_id])
-  end
+  # def load_event
+  #   @event = Event.find(params[:event_id])
+  # end
 
   def voter_params
-    params.require(:voter).permit(:name)
+    params.require(:voter).permit(:name, :event_id)
   end
 end
